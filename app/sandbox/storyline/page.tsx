912f344776d3a0b28f7b41b3862d9328217712f3
Ana Maria Luca <popescu.anamaria93@gmail.com>
2024-01-06 14:52:23 +0200

    Added planet.

--- app/sandbox/storyline/page.tsx
+++ app/sandbox/storyline/page.tsx
@@ -20,12 +20,16 @@ function Storyline() {
   const target = useRef(null);
 
   const floating_person = useParallax({
-    speed: -60,
-    // targetElement: target.current,
+    speed: -40,
   });
-  const train = useParallax({
-    speed: 90,
-    // targetElement: target.current,
+  const planet = useParallax({
+    speed: 20,
+  });
+  const planet_scale = useParallax({
+    scale: [0, 1, "easeInQuad"],
+  });
+  const planet_rotate = useParallax({
+    rotate: [0, 360],
   });
 
   return (
@@ -41,7 +45,7 @@ function Storyline() {
     >
       <div
         ref={floating_person.ref as React.RefObject<HTMLDivElement>}
-        className="absolute bottom-[3%] left-[80%]"
+        className="absolute bottom-[3%] left-[-10%]"
       >
         <Image
           src="/storyline/floating_person.png"
@@ -51,13 +55,31 @@ function Storyline() {
         />
       </div>
 
-      <div ref={train.ref as React.RefObject<HTMLDivElement>} 
-           className="absolute bottom-[35%] left-[55%]">
+      <div ref={planet.ref as React.RefObject<HTMLDivElement>} 
+           className="absolute bottom-[35%] left-[80%]">
         <Image
           src="/storyline/planet.png"
           alt="planet"
           height={35}
           width={35}
+        />
+      </div>
+      <div ref={planet_scale.ref as React.RefObject<HTMLDivElement>} 
+           className="absolute bottom-[24%] left-[75%] opacity-70">
+        <Image
+          src="/storyline/planet.png"
+          alt="planet"
+          height={45}
+          width={45}
+        />
+      </div>
+      <div ref={planet_rotate.ref as React.RefObject<HTMLDivElement>} 
+           className="absolute bottom-[45%] left-[70%]">
+        <Image
+          src="/storyline/planet.png"
+          alt="planet"
+          height={75}
+          width={75}
         />
       </div>
     </div>
