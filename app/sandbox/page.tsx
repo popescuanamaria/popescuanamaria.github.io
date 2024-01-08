31b2515800360dcf10ee9c3d06c6954902a5c9dd
Ana Maria Luca <popescu.anamaria93@gmail.com>
2024-01-08 23:20:46 +0200

    Added back button.

--- app/sandbox/page.tsx
+++ app/sandbox/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 
 import Link from "next/link";
+import { BackArrow } from "@/components/Icons";
 
 export default function ProjectsMenu() {
   return (
     <div className="flex justify-center w-full">
-      <div className="flex flex-col items-start w-full pt-20 pl-20">
-        <ul className="list-disc">
-          <li>
-            <Link href="sandbox/storyline">Storyline</Link>
-          </li>
-        </ul>
+      <div className="flex flex-col items-start w-full pt-20 pl-20 gap-y-20">
+        <div className="opacity-50">
+          <Link href="/about">
+            <BackArrow />
+          </Link>
+        </div>
+        <div className="pl-5">
+          <ul className="list-disc">
+            <li>
+              <Link href="sandbox/storyline">Storyline</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-  </div>
+    </div>
   );
 }
--- app/sandbox/storyline/page.tsx
+++ app/sandbox/storyline/page.tsx
@@ -23,7 +23,10 @@ function Storyline() {
     speed: -40,
   });
   const planet = useParallax({
-    speed: 20,
+    easing: "easeInQuad",
+    scale: [0, 1],
+    translateX: [0, 260],
+    translateY: [0, -300],
   });
   const planet_scale = useParallax({
     scale: [0, 1, "easeInQuad"],
@@ -73,147 +76,149 @@ function Storyline() {
   });
 
   return (
-    <div
-      ref={target}
-      style={{
-        backgroundImage: "url('/storyline/base_image.png')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        width: "5500px",
-      }}
-      className="h-screen"
-    >
+    <div className="w-full h-full">
       <div
-        ref={floating_person.ref as React.RefObject<HTMLDivElement>}
-        className="absolute bottom-[3%] left-[-10%]"
+        ref={target}
+        style={{
+          backgroundImage: "url('/storyline/base_image.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          width: "5500px",
+        }}
+        className="hidden lg:flex lg:h-screen"
       >
-        <Image
-          src="/storyline/floating_person.png"
-          alt="floating person"
-          height={40}
-          width={354}
-        />
-      </div>
-      <div
-        ref={planet.ref as React.RefObject<HTMLDivElement>}
-        className="absolute bottom-[35%] left-[80%]"
-      >
-        <Image
-          src="/storyline/planet.png"
-          alt="planet"
-          height={35}
-          width={35}
-        />
-      </div>
-      <div
-        ref={planet_scale.ref as React.RefObject<HTMLDivElement>}
-        className="absolute bottom-[24%] left-[75%] opacity-70"
-      >
-        <Image
-          src="/storyline/planet.png"
-          alt="planet"
-          height={45}
-          width={45}
-        />
-      </div>
-      <div
-        ref={planet_rotate.ref as React.RefObject<HTMLDivElement>}
-        className="absolute bottom-[45%] left-[70%]"
-      >
-        <Image
-          src="/storyline/planet.png"
-          alt="planet"
-          height={75}
-          width={75}
-        />
-      </div>
-      <div
-        ref={birdup0.ref as React.RefObject<HTMLDivElement>}
-        className="absolute top-[5%] right-[-170%]"
-      >
-        <Image
-          src="/storyline/bird_up.png"
-          alt="planet"
-          height={35}
-          width={35}
-        />
-      </div>
-      <div
-        ref={birdup1.ref as React.RefObject<HTMLDivElement>}
-        className="absolute top-[5%] right-[-180%]"
-      >
-        <Image
-          src="/storyline/bird_up.png"
-          alt="planet"
-          height={35}
-          width={35}
-        />
-      </div>
-      <div
-        ref={birdup2.ref as React.RefObject<HTMLDivElement>}
-        className="absolute top-[7%] right-[-206%]"
-      >
-        <Image
-          src="/storyline/bird_up.png"
-          alt="planet"
-          height={35}
-          width={35}
-        />
-      </div>
-      <div
-        ref={birdup3.ref as React.RefObject<HTMLDivElement>}
-        className="absolute top-[7%] right-[-230%]"
-      >
-        <Image
-          src="/storyline/bird_up.png"
-          alt="planet"
-          height={35}
-          width={35}
-        />
-      </div>
-      <div
-        ref={birddown0.ref as React.RefObject<HTMLDivElement>}
-        className="absolute top-[5%] right-[-200%]"
-      >
-        <Image
-          src="/storyline/bird_down.png"
-          alt="planet"
-          height={35}
-          width={35}
-        />
-      </div>
-      <div
-        ref={birddown1.ref as React.RefObject<HTMLDivElement>}
-        className="absolute top-[27%] right-[-190%]"
-      >
-        <Image
-          src="/storyline/bird_down.png"
-          alt="planet"
-          height={15}
-          width={15}
-        />
-      </div>
-      <div
-        ref={birddown2.ref as React.RefObject<HTMLDivElement>}
-        className="absolute top-[27%] right-[-170%]"
-      >
-        <Image
-          src="/storyline/bird_down.png"
-          alt="planet"
-          height={15}
-          width={15}
-        />
-      </div>
-      <div
-        ref={birddown3.ref as React.RefObject<HTMLDivElement>}
-        className="absolute top-[23%] right-[-230%]"
-      >
-        <Image
-          src="/storyline/bird_down.png"
-          alt="planet"
-          height={15}
-          width={15}
-        />
+        <div
+          ref={floating_person.ref as React.RefObject<HTMLDivElement>}
+          className="absolute bottom-[3%] left-[-10%]"
+        >
+          <Image
+            src="/storyline/floating_person.png"
+            alt="floating person"
+            height={40}
+            width={354}
+          />
+        </div>
+        <div
+          ref={planet.ref as React.RefObject<HTMLDivElement>}
+          className="absolute bottom-[20%] left-[75%]"
+        >
+          <Image
+            src="/storyline/planet.png"
+            alt="planet"
+            height={35}
+            width={35}
+          />
+        </div>
+        <div
+          ref={planet_scale.ref as React.RefObject<HTMLDivElement>}
+          className="absolute bottom-[24%] left-[75%] opacity-70"
+        >
+          <Image
+            src="/storyline/planet.png"
+            alt="planet"
+            height={45}
+            width={45}
+          />
+        </div>
+        <div
+          ref={planet_rotate.ref as React.RefObject<HTMLDivElement>}
+          className="absolute bottom-[45%] left-[70%]"
+        >
+          <Image
+            src="/storyline/planet.png"
+            alt="planet"
+            height={75}
+            width={75}
+          />
+        </div>
+        <div
+          ref={birdup0.ref as React.RefObject<HTMLDivElement>}
+          className="absolute top-[5%] right-[-170%]"
+        >
+          <Image
+            src="/storyline/bird_up.png"
+            alt="planet"
+            height={35}
+            width={35}
+          />
+        </div>
+        <div
+          ref={birdup1.ref as React.RefObject<HTMLDivElement>}
+          className="absolute top-[5%] right-[-180%]"
+        >
+          <Image
+            src="/storyline/bird_up.png"
+            alt="planet"
+            height={35}
+            width={35}
+          />
+        </div>
+        <div
+          ref={birdup2.ref as React.RefObject<HTMLDivElement>}
+          className="absolute top-[7%] right-[-206%]"
+        >
+          <Image
+            src="/storyline/bird_up.png"
+            alt="planet"
+            height={35}
+            width={35}
+          />
+        </div>
+        <div
+          ref={birdup3.ref as React.RefObject<HTMLDivElement>}
+          className="absolute top-[7%] right-[-230%]"
+        >
+          <Image
+            src="/storyline/bird_up.png"
+            alt="planet"
+            height={35}
+            width={35}
+          />
+        </div>
+        <div
+          ref={birddown0.ref as React.RefObject<HTMLDivElement>}
+          className="absolute top-[5%] right-[-200%]"
+        >
+          <Image
+            src="/storyline/bird_down.png"
+            alt="planet"
+            height={35}
+            width={35}
+          />
+        </div>
+        <div
+          ref={birddown1.ref as React.RefObject<HTMLDivElement>}
+          className="absolute top-[27%] right-[-190%]"
+        >
+          <Image
+            src="/storyline/bird_down.png"
+            alt="planet"
+            height={15}
+            width={15}
+          />
+        </div>
+        <div
+          ref={birddown2.ref as React.RefObject<HTMLDivElement>}
+          className="absolute top-[27%] right-[-170%]"
+        >
+          <Image
+            src="/storyline/bird_down.png"
+            alt="planet"
+            height={15}
+            width={15}
+          />
+        </div>
+        <div
+          ref={birddown3.ref as React.RefObject<HTMLDivElement>}
+          className="absolute top-[23%] right-[-230%]"
+        >
+          <Image
+            src="/storyline/bird_down.png"
+            alt="planet"
+            height={15}
+            width={15}
+          />
+        </div>
       </div>
     </div>
   );
