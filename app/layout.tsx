43cdef7495ed32a4ec6fb524d39832281eefe730
Ana Maria Luca <popescu.anamaria93@gmail.com>
2024-01-30 15:58:51 +0200

    Moved footer and header in layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Providers from "./providers";
 
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,9 +21,125 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
-          <div className="flex h-full bg-zinc-50 dark:bg-[#202020]">
-            {children}
-          </div>
+          <header
+            className="pointer-events-none relative z-50 flex flex-none flex-col"
+            style={{
+              height: "var(--header-high)",
+              marginBottom: "var(--header-mb)",
+            }}
+          >
+            <div
+              className="top-0 z-10 h-16 pt-6"
+              style={{
+                position: "var(--header-position)",
+              }}
+            >
+              <div
+                className="sm:px-8 top-[var(--header-top, theme(spacing,6))] w-full"
+                style={{
+                  position: "var(--header-inner-position)",
+                }}
+              >
+                <div className="mx-auto w-full max-w-7xl lg:px-8">
+                  <div className="relative px-4 sm:px-8 lg:px-12">
+                    <div className="mx-auto max-w-2xl lg:max-w-5xl">
+                      <div className="relative flex gap-4 justify-evenly">
+                        <div className="flex flex-1">
+                          <div className="h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10">
+                            <a
+                              aria-label="Home"
+                              className="pointer-events-auto"
+                              href="/"
+                            >
+                              <img
+                                alt="profile_img"
+                                decoding="async"
+                                data-nimg="1"
+                                className="rounded-full bg-zinc-100 object-cover dark:bg-zinc-800 h-9 w-9"
+                                style={{ color: "transparent" }}
+                                sizes="2.25rem"
+                                src="/profile/profile_image.jpeg"
+                              ></img>
+                              {/* <img alt="" fetchpriority="high" width="512" height="512" decoding="async" data-nimg="1" class="rounded-full bg-zinc-100 object-cover dark:bg-zinc-800 h-9 w-9" style={{color: "transparent" }} sizes="2.25rem" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=16&amp;q=75 16w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=32&amp;q=75 32w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=48&amp;q=75 48w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=64&amp;q=75 64w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=96&amp;q=75 96w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=128&amp;q=75 128w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=256&amp;q=75 256w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=384&amp;q=75 384w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=640&amp;q=75 640w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=750&amp;q=75 750w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=828&amp;q=75 828w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=1080&amp;q=75 1080w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=1200&amp;q=75 1200w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=1920&amp;q=75 1920w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=2048&amp;q=75 2048w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=3840&amp;q=75 3840w" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.51a13c67.jpg&amp;w=3840&amp;q=75"> */}
+                            </a>
+                          </div>
+                        </div>
+                        <div className="flex flex-1 justify-end">
+                          {/*md:justify-center*/}
+                          <nav className="pointer-events-auto">
+                            <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+                              <li>
+                                <a
+                                  className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
+                                  href="/"
+                                >
+                                  Home
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
+                                  href="/resume"
+                                >
+                                  Resume
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
+                                  href="/sandbox"
+                                >
+                                  Projects
+                                </a>
+                              </li>
+                            </ul>
+                          </nav>
+                        </div>
+                        <div className="flex flex-1 justify-end">
+                          <ThemeSwitcher />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </header>
+          {children}
+          <footer className="mt-32 flex-none">
+            <div className="sm:px-8">
+              <div className="mx-auto w-full max-w-7xl lg:px-8">
+                <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
+                  <div className="relative px-4 sm:px-8 lg:px-12">
+                    <div className="mx-auto max-w-2xl lg:max-w-5xl">
+                      <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+                        <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+                          <a
+                            className="transition hover:text-teal-500 dark:hover:text-teal-400"
+                            href="/home"
+                          >
+                            Home
+                          </a>
+                          <a
+                            className="transition hover:text-teal-500 dark:hover:text-teal-400"
+                            href="/resume"
+                          >
+                            Resume
+                          </a>
+                          <a
+                            className="transition hover:text-teal-500 dark:hover:text-teal-400"
+                            href="/projects"
+                          >
+                            Projects
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </footer>
         </Providers>
       </body>
     </html>
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,152 +1,156 @@
 "use client";
 
 import TopBar from "@/components/TopBar";
-
+import clsx from "clsx";
 
 export default function Resume() {
   return (
-    <div className="flex flex-col min-h-screen text-lg text-[#888888]">
-    <TopBar />
+    <div className={clsx("flex flex-col min-h-screen text-lg text-[#888888]")}>
+      <TopBar />
 
-    <div className="flex-1 h-full">
-    <div className="flex justify-center w-full">
-      <div className="flex flex-col items-center w-full sm:max-w-4xl pt-5 pb-20 gap-y-10">
-        <div className="flex sm:justify-start w-full">
-          <div className="flex flex-col items-end pl-3 pr-3 w-3/4 border-r-2 border-[#888888] border-opacity-50 sm:w-1/2">
-            <div className="uppercase font-normal text-lg">contact</div>
-            <br></br>
-            <div className="font-thin text-lg">(+45) 12 34 56 78</div>
-            <div className="font-thin text-lg">
-              popescu.anamaria93@gmail.com
+      {/* <div className="flex-1 h-full">
+        <div className="flex justify-center w-full">
+          <div className="flex flex-col items-center w-full sm:max-w-4xl pt-5 pb-20 gap-y-10">
+            <div className="flex sm:justify-start w-full">
+              <div className="flex flex-col items-end pl-3 pr-3 w-3/4 border-r-2 border-[#888888] border-opacity-50 sm:w-1/2">
+                <div className="uppercase font-normal text-lg">contact</div>
+                <br></br>
+                <div className="font-thin text-lg">(+45) 12 34 56 78</div>
+                <div className="font-thin text-lg">
+                  popescu.anamaria93@gmail.com
+                </div>
+                <div className="font-thin text-lg">Copenhagen</div>
+              </div>
             </div>
-            <div className="font-thin text-lg">Copenhagen</div>
+
+            <div className="flex justify-end w-full">
+              <div className="flex flex-col items-start pl-3 pr-3 w-3/4 border-l-2 border-[#888888] border-opacity-50 sm:w-1/2">
+                <div className="uppercase font-normal text-lg">Education</div>
+                <br></br>
+                <div className="font-thin text-lg">Bachelor of SCIENCE</div>
+                <div className="font-thin text-lg">Computer Science</div>
+                <div className="font-thin text-lg">
+                  Politehnica University Timisoara
+                </div>
+                <div className="font-normal text-lg opacity-70">
+                  2011 - 2015
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-start w-full">
+              <div className="flex flex-col items-end pl-6 pr-3 w-3/4 border-r-2 border-[#888888] border-opacity-50 sm:w-1/2">
+                <div className="uppercase font-normal text-lg">Experience</div>
+                <br></br>
+                <div className="uppercase font-normal text-lg opacity-70 pb-3">
+                  Software developer intern
+                </div>
+                <div className="font-normal text-lg pb-3">Movidius</div>
+                <div className="font-normal text-lg opacity-70 pb-3">
+                  June 2013 - Octomber 2013
+                </div>
+                <ul className="list-disc">
+                  <li>
+                    Participated in an HDR project utilizing a designated image
+                    sensor. The objective was to achieve the highest-quality
+                    image, and my responsibilities included conducting research
+                    and analyzing outcomes in various scenarios. This involved
+                    incorporating new implementations for different filters in
+                    the pipeline, adjusting parameters, and more.
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="flex justify-end w-full">
+              <div className="flex flex-col items-start pl-6 pr-3 w-3/4 border-l-2 border-[#888888] border-opacity-50 sm:w-1/2">
+                <div className="uppercase font-normal text-lg opacity-70 pb-3">
+                  Software developer
+                </div>
+                <div className="font-normal text-lg pb-3">Movidius</div>
+                <div className="font-normal text-lg opacity-70 pb-3">
+                  Octomber 2013 - November 2016
+                </div>
+                <ul className="list-disc">
+                  <li>
+                    Developed diverse image processing filters in C and
+                    optimized them using assembly language for our Movidius
+                    proprietary processor.
+                  </li>
+                  <li>
+                    Collaborated closely with the support team to enhance
+                    projects for diverse clients, incorporating new filters and
+                    optimizing code in assembly language. Additionally, provided
+                    on-site support for clients, ensuring seamless integration
+                    and optimal performance of the implemented enhancements.
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="flex justify-start w-full">
+              <div className="flex flex-col items-end pl-6 pr-2 w-3/4 border-r-2 border-[#888888] border-opacity-50 sm:w-1/2">
+                <div className="uppercase font-normal text-lg opacity-70 pb-3">
+                  Software Engineer
+                </div>
+                <div className="font-normal text-lg pb-3">
+                  Intel Corporation
+                </div>
+                <div className="font-normal text-lg opacity-70 pb-3">
+                  November 2016 - Present
+                </div>
+                <ul className="list-disc">
+                  <li>
+                    Engineered a C++ model designed to simulate a dedicated CNN
+                    (Convolutional Neural Network) hardware block for
+                    verification purposes, with a specific emphasis on
+                    byte-level transactions. The model was required to simulate
+                    each sub-block, along with handling transactions between
+                    sub-blocks and interactions between memory and the CNN unit.
+                  </li>
+                  <li>
+                    Integrated various computer vision hardware blocks into our
+                    proprietary architecture, enhancing existing blocks with
+                    additional features (e.g. Stereo, Matrix Multiplication,
+                    Event Processing Unit). Conducted low-level verification
+                    using SystemVerilog.
+                  </li>
+                  <li>
+                    Extended our Python and C++ compiler for CNN models with new
+                    operations. Integrated an arithmetic emulator for reference
+                    result validation and utilized the compiler to generate
+                    diverse datasets for power and performance measurements on a
+                    specific neural network hardware block.
+                  </li>
+                  <li>
+                    Actively mentored junior engineers across a wide spectrum of
+                    tasks, including technical onboarding, coding and debugging.
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="flex justify-end w-full">
+              <div className="flex flex-col items-start pl-6 pr-2 w-3/4 border-l-2 border-[#888888] border-opacity-50 sm:w-1/2">
+                <div className="uppercase font-normal text-lg">
+                  Technical skills
+                </div>
+                <br></br>
+                <ul className="list-disc">
+                  <li>
+                    Programming languages: C, C++, Python, Verilog, Assembly
+                    language
+                  </li>
+                  <li>Version control: Git, SVN</li>
+                  <li>Deep Learning frameworks: TensorFlow, PyTorch</li>
+                  <li>Algorithm development and optimization</li>
+                  <li>Agile methodologies</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className="flex justify-end w-full">
-          <div className="flex flex-col items-start pl-3 pr-3 w-3/4 border-l-2 border-[#888888] border-opacity-50 sm:w-1/2">
-            <div className="uppercase font-normal text-lg">Education</div>
-            <br></br>
-            <div className="font-thin text-lg">Bachelor of SCIENCE</div>
-            <div className="font-thin text-lg">Computer Science</div>
-            <div className="font-thin text-lg">
-              Politehnica University Timisoara
-            </div>
-            <div className="font-normal text-lg opacity-70">2011 - 2015</div>
-          </div>
-        </div>
-
-        <div className="flex justify-start w-full">
-          <div className="flex flex-col items-end pl-6 pr-3 w-3/4 border-r-2 border-[#888888] border-opacity-50 sm:w-1/2">
-            <div className="uppercase font-normal text-lg">Experience</div>
-            <br></br>
-            <div className="uppercase font-normal text-lg opacity-70 pb-3">
-              Software developer intern
-            </div>
-            <div className="font-normal text-lg pb-3">Movidius</div>
-            <div className="font-normal text-lg opacity-70 pb-3">
-              June 2013 - Octomber 2013
-            </div>
-            <ul className="list-disc">
-              <li>
-                Participated in an HDR project utilizing a designated image
-                sensor. The objective was to achieve the highest-quality image,
-                and my responsibilities included conducting research and
-                analyzing outcomes in various scenarios. This involved
-                incorporating new implementations for different filters in the
-                pipeline, adjusting parameters, and more.
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="flex justify-end w-full">
-          <div className="flex flex-col items-start pl-6 pr-3 w-3/4 border-l-2 border-[#888888] border-opacity-50 sm:w-1/2">
-            <div className="uppercase font-normal text-lg opacity-70 pb-3">
-              Software developer
-            </div>
-            <div className="font-normal text-lg pb-3">Movidius</div>
-            <div className="font-normal text-lg opacity-70 pb-3">
-              Octomber 2013 - November 2016
-            </div>
-            <ul className="list-disc">
-              <li>
-                Developed diverse image processing filters in C and optimized
-                them using assembly language for our Movidius proprietary
-                processor.
-              </li>
-              <li>
-                Collaborated closely with the support team to enhance projects
-                for diverse clients, incorporating new filters and optimizing
-                code in assembly language. Additionally, provided on-site
-                support for clients, ensuring seamless integration and optimal
-                performance of the implemented enhancements.
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="flex justify-start w-full">
-          <div className="flex flex-col items-end pl-6 pr-2 w-3/4 border-r-2 border-[#888888] border-opacity-50 sm:w-1/2">
-            <div className="uppercase font-normal text-lg opacity-70 pb-3">
-              Software Engineer
-            </div>
-            <div className="font-normal text-lg pb-3">Intel Corporation</div>
-            <div className="font-normal text-lg opacity-70 pb-3">
-              November 2016 - Present
-            </div>
-            <ul className="list-disc">
-              <li>
-                Engineered a C++ model designed to simulate a dedicated CNN
-                (Convolutional Neural Network) hardware block for verification
-                purposes, with a specific emphasis on byte-level transactions.
-                The model was required to simulate each sub-block, along with
-                handling transactions between sub-blocks and interactions
-                between memory and the CNN unit.
-              </li>
-              <li>
-                Integrated various computer vision hardware blocks into our
-                proprietary architecture, enhancing existing blocks with
-                additional features (e.g. Stereo, Matrix Multiplication, Event
-                Processing Unit). Conducted low-level verification using
-                SystemVerilog.
-              </li>
-              <li>
-                Extended our Python and C++ compiler for CNN models with new
-                operations. Integrated an arithmetic emulator for reference
-                result validation and utilized the compiler to generate diverse
-                datasets for power and performance measurements on a specific
-                neural network hardware block.
-              </li>
-              <li>
-                Actively mentored junior engineers across a wide spectrum of
-                tasks, including technical onboarding, coding and debugging.
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="flex justify-end w-full">
-          <div className="flex flex-col items-start pl-6 pr-2 w-3/4 border-l-2 border-[#888888] border-opacity-50 sm:w-1/2">
-            <div className="uppercase font-normal text-lg">
-              Technical skills
-            </div>
-            <br></br>
-            <ul className="list-disc">
-              <li>
-                Programming languages: C, C++, Python, Verilog, Assembly
-                language
-              </li>
-              <li>Version control: Git, SVN</li>
-              <li>Deep Learning frameworks: TensorFlow, PyTorch</li>
-              <li>Algorithm development and optimization</li>
-              <li>Agile methodologies</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
+      </div> */}
     </div>
   );
 }
